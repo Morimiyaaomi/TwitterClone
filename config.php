f9abce9cb71e8727727020bcdcf55bdb59410a2e
kobori <riggkobo8@gmail.com>
2023-06-02 21:25:03 +0900

    会員登録機能

--- config.php
+++ config.php
@@ -6,3 +6,8 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定 置換しておくと後からURLを変更しなければならない時に
 //  下記URLを変更すると全ての定数に反映される。
 define('HOME_URL', '/TwitterClone/');
+// データベースの接続情報の設定
+define('DB_HOST' , 'localhost');
+define('DB_USER' , 'root');
+define('DB_PASSWORD' , 'root');
+define('DB_NAME' , 'twitter_clone');
