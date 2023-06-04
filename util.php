c09c1a89c1c5e1f31c61e0c72df43ee283ce2539
kobori <riggkobo8@gmail.com>
2023-06-04 13:58:02 +0900

    ログイン機能

--- util.php
+++ util.php
@@ -57,3 +57,64 @@ function convertToDayTimeAgo(string $datetime)
 
   return (int)$time . $unit; //カタキャスト:int:intで表せない値は0,小数点以下は切捨て
 }
+
+/**
+ * ユーザー情報をセッションに保存する
+ * 
+ * @param array $user
+ * @return void
+ */
+function saveUserSession(array $user)
+{
+  // セッションを開始していない場合
+  if (session_status() === PHP_SESSION_NONE) {
+    // セッション開始
+    session_start();
+  }
+
+  $_SESSION['USER'] = $user;
+}
+/**
+ * ユーザー情報をセッションから削除する
+ * 
+ * @return void
+ */
+function deleteUserSession()
+{
+  // セッションを開始していない場合
+  if (session_status() === PHP_SESSION_NONE) {
+    // セッション開始
+    session_start();
+  }
+
+  // セッションのユーザー情報を削除
+  unset($_SESSION['USER']);
+}
+
+/**
+ * セッションのユーザー情報を取得
+ * 
+ * @return array|false
+ */
+function getUserSession()
+{
+  // セッションを開始していない場合
+  if (session_status() === PHP_SESSION_NONE) {
+    // セッション開始
+    session_start();
+  }
+  if (!isset($_SESSION['USER'])) {
+    // セッションにユーザー情報がない場合
+    return false;
+  }
+
+  $user = $_SESSION['USER'];
+
+  // 画像のファイル名からファイルのURLを取得
+  if (!isset($user['image_name'])) {
+      $user['image_name'] = null;
+  }
+  $user['image_path'] = buildImagePath($user['image_name'], 'user');
+
+  return $user;
+}
